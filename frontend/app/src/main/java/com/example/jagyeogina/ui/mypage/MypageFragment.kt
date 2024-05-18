4a5d7fe194fd462729ed06bb4beb13c64d5908d6
HJTN <zintnm21@gmail.com>
2024-05-18 21:49:37 +0900

    C: Home Page View

---
+++ frontend/app/src/main/java/com/example/jagyeogina/ui/mypage/MypageFragment.kt
@@ -0,0 +1,34 @@
+package com.example.jagyeogina.ui.mypage
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.jagyeogina.databinding.FragmentGuestHomeBinding
+import com.example.jagyeogina.databinding.FragmentMypageBinding
+
+class MypageFragment : Fragment() {
+    lateinit var binding: FragmentMypageBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMypageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUi()
+    }
+
+    private fun setUi() {
+        binding.apply {
+
+        }
+    }
+}
---
+++ frontend/app/src/main/java/com/example/jagyeogina/ui/search/SearchFragment.kt
@@ -0,0 +1,34 @@
+package com.example.jagyeogina.ui.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.jagyeogina.databinding.FragmentGuestHomeBinding
+import com.example.jagyeogina.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+    lateinit var binding: FragmentSearchBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSearchBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUi()
+    }
+
+    private fun setUi() {
+        binding.apply {
+
+        }
+    }
+}
